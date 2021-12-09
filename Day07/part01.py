d4a22425ccc5459352b1990caf250f46b9184964
Giorgio Ceolin <g.ceolin@gmail.com>
2021-12-09 21:59:15 +0000

    Day 07 Part 2

--- Day07/part01.py
+++ Day07/part01.py
@@ -7,7 +7,7 @@ with open('Day07/input.txt', 'r') as f:
 
 source = input_data
 
-def fuel_consumed(state, position):
+def fuel_consumed_part1(state, position):
     return sum(abs(point - position) for point in state)
 
 
@@ -16,18 +16,28 @@ ordered_points = set(input_data)
 start_index = 0
 end_index = max(ordered_points) 
 
-while True:
-    middle_index = start_index + ((end_index - start_index) // 2)
-    middle_fuel = fuel_consumed(input_data, middle_index)
-    middle_fuel_next = fuel_consumed(input_data, middle_index+1)
-    if middle_fuel > middle_fuel_next:
-        start_index = middle_index + 1
-    else:
-        end_index = middle_index
-    if start_index == end_index:
-        break
+def solver(input_data, start_index, end_index, fuel_calculator):
+    while True:
+        middle_index = start_index + ((end_index - start_index) // 2)
+        middle_fuel = fuel_calculator(input_data, middle_index)
+        middle_fuel_next = fuel_calculator(input_data, middle_index+1)
+        if middle_fuel > middle_fuel_next:
+            start_index = middle_index + 1
+        else:
+            end_index = middle_index
+        if start_index == end_index:
+            break
+    return fuel_calculator(input_data, start_index)
 
-final_fuel = fuel_consumed(input_data, start_index)
+final_fuel = solver(input_data, start_index, end_index, fuel_consumed_part1)
 print(f'Day07; Part1; Minimum fuel consumed: {final_fuel}')
         
+def fuel_consumed_part2(state, position):
+    total = 0
+    for point in state:
+        d = abs(point - position)
+        total += d * (d + 1) // 2
+    return total
     
+final_fuel = solver(input_data, start_index, end_index, fuel_consumed_part2)
+print(f'Day07; Part2; Minimum fuel consumed: {final_fuel}')
