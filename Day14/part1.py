336d03cf52a7c27e7beebc0b6ebeefccb59ee93f
Giorgio Ceolin <g.ceolin@gmail.com>
2022-01-08 18:36:35 +0000

    Day 14 done

--- Day14/part1.py
+++ Day14/part1.py
@@ -1,7 +1,9 @@
-# filename = 'Day14/input.txt'
-filename = 'Day14/test.txt'
+from functools import lru_cache
 
-steps = 10
+filename = 'Day14/input.txt'
+# filename = 'Day14/test.txt'
+
+total_steps = 40
 
 with open(filename, 'r') as f:
     chain = f.readline().strip() #.split()
@@ -12,22 +14,39 @@ with open(filename, 'r') as f:
         a, b = line.strip().split(' -> ')
         rules[(a[0], a[1])] = b
 
-for _ in range(steps):
-    new_chain = [chain[0]]
+def combine(a, b):
+    c = {}
+    a_values = list(a.keys())
+    b_values = list(b.keys())
+    for i in (a_values + b_values):
+        if i in a_values and i in b_values:
+            c[i] = a[i] + b[i]
+        elif i in a_values:
+            c[i] = a[i]
+        else:
+            c[i] = b[i]
+    return c
+
+cache = {}
+
+def expand(tup, steps):
+    if (tup, steps) in cache:
+        return cache[(tup, steps)]
+    if steps == 0 or tup not in rules:
+        return {tup[1]: 1}
+    insertion = rules[tup]
+    left = expand((tup[0], insertion), steps-1)
+    right = expand((insertion, tup[1]), steps-1)
+    total = combine(left, right)
+    cache[(tup, steps)] = total
+    return total
+
+def solution(total_steps):
+    counter = {chain[0]: 1}
     for i in range(0, len(chain) - 1):
         element = (chain[i], chain[i+1])
-        if element in rules:
-            new_chain.append(rules[element])
-            new_chain.append(chain[i+1])
-        else:
-            new_chain.append(chain[i+1])
-    chain = new_chain
+        counter = combine(counter, expand(element, total_steps))
+    return max(counter.values()) - min(counter.values())
 
-counter = {}
-for c in chain:
-    if c in counter:
-        counter[c] += 1
-    else:
-        counter[c] = 1
-
-print(f'Day 14; Part 1; Solution: {max(counter.values()) - min(counter.values())}')
+print(f'Day 14; Part 1; Solution: {solution(10)}')
+print(f'Day 14; Part 2; Solution: {solution(40)}')
